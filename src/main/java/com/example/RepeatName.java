212138795fd93b8ce32480a9b2372d1a30cc8fc7
nithinburugu <burugunithin03@gmail.com>
2024-05-30 08:44:39 +0530

    commit the changes

--- src/main/java/com/example/RepeatName.java
+++ src/main/java/com/example/RepeatName.java
@@ -14,18 +14,22 @@ public class RepeatName {
         name = ob.nextLine();
         System.out.println("How many times you want to repeat: ");
         int n = ob.nextInt();
+        System.out.println("-------------------------------");
         for (int i = 1; i <= n; i++) {
             System.out.println(name + " " + i);
         }
+        System.out.println("-------------------------------");
         int i = 1;
         while (i <= n) {
             System.out.println(name + " " + i);
             i++;
         }
+        System.out.println("-------------------------------");
         int cnt = 1;
         do {
             System.out.println(name + " " + cnt);
             cnt++;
         } while (cnt <= n);
+        System.out.println("-------------------------------");
     }
 }
